d50ca52d0a3f976b8fca4c10e89b78c8928eac02
Chen Yin <yinchen0621@foxmail.com>
2022-06-10 19:40:45 +0800

    [Misc] Support container template

--- DataflowSim/src/util/ReadFile.hpp
+++ DataflowSim/src/util/ReadFile.hpp
@@ -8,18 +8,14 @@ namespace DFSim
     class ReadFile
     {
     public:
-        template<typename Type>
+        /*template<typename Type>
         static void readFile2UnifiedVector(vector<Type>& vec, ifstream& infile)
         {
-            //uint k = 0;
             string temp_str;
             while (getline(infile, temp_str))
             {
                 Type temp_data = std::stoi(temp_str);
-                //vec[k] = temp_data;
                 vec.push_back(temp_data);
-                //Simulator::Array::MemoryData::getInstance()->setInitialized(k);
-                //k++;
             }
         }
 
@@ -30,6 +26,27 @@ namespace DFSim
             infile.open(filePath);
             DEBUG_ASSERT(infile.is_open());
             ReadFile::readFile2UnifiedVector(vec, infile);
+            infile.close();
+        }*/
+
+        template<class Container>
+        static void readFile2Container(typename Container& container, ifstream& infile)
+        {
+            string temp_str;
+            while (getline(infile, temp_str))
+            {
+                typename Container::value_type temp_data = std::stoi(temp_str);
+                container.push_back(temp_data);
+            }
+        }
+
+        template<class Container>
+        static void readFile(typename Container& container, const string& filePath)
+        {
+            ifstream infile;
+            infile.open(filePath);
+            DEBUG_ASSERT(infile.is_open());
+            ReadFile::readFile2Container(container, infile);
             infile.close();
         }
     };
